28760ffdc29d61c0f284fb214ce31a9ce7849344
Dave Brown <daveb.za@gmail.com>
2017-02-17 09:44:14 +0800

    Added formatting rules for HTMLheaderName and HTMLheaderRole to insert real values.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+*/
+var formattedName = HTMLheaderName.replace("%data%", "Dave Brown");
+var formattedRole = HTMLheaderRole.replace("%data%","FrontEnd Ninja");
 
- $("#main").append("Dave Brown");
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
